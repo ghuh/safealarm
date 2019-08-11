61bb4370b53a398d2163f64941fccf9046373f89
kphayen <kphayen@gmail.com>
2019-08-10 20:49:53 -0600

    Retry messages

--- message.go
+++ message.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-    "github.com/mailjet/mailjet-apiv3-go"
-    "log"
+	"fmt"
+	"log"
+
+	"github.com/mailjet/mailjet-apiv3-go"
 )
 
 // https://icyapril.com/go/programming/2017/12/17/object-orientation-in-go.html
@@ -11,78 +13,79 @@ import (
 const numRetries int = 1
 
 type Message struct {
-    config Config
+	config Config
 }
 
 // NewMessage creates a new object for sending email messages.
 func NewMessage(config Config) Message {
-    message := Message{config}
-    return message
+	message := Message{config}
+	return message
 }
 
 // SendStarting sends a message that the program just started which also indicates that the machine just powered up.
 func (m Message) SendStarting() {
-    m.sendMessage("Safe Alarm Powering Up")
+	m.sendMessage("Safe Alarm Powering Up")
 }
 
 // SendOpen sends a message that the sensor is reading open.
 func (m Message) SendOpen() {
-    m.sendMessage("Safe Open")
+	m.sendMessage("Safe Open")
 }
 
 // SendClosed sends a message that the sensor is reading closed.
 func (m Message) SendClosed() {
-    m.sendMessage("Safe Closed")
+	m.sendMessage("Safe Closed")
 }
 
 // SendForgot sends a message that the sensor was left open.
 func (m Message) SendForgot() {
-    m.sendMessage("Safe Left Open")
+	m.sendMessage("Safe Left Open")
 }
 
 // Heartbeat sends a message that the program is still running
 func (m Message) Heartbeat() {
-    m.sendMessage("Safe Heartbeat")
+	m.sendMessage("Safe Heartbeat")
 }
 
 // sendMessage sends the given message to all email addresses in the config file using mailjet.
 // https://app.mailjet.com/transactional/sendapi
 // https://github.com/mailjet/mailjet-apiv3-go
 func (m Message) sendMessage(message string) {
-    log.Printf("Sending message [ %v ]", message)
+	log.Printf("Sending message [ %v ]", message)
 
-    // https://stackoverflow.com/a/38362784/10788820
-    recipients := make([]mailjet.Recipient, len(m.config.TargetEmails))
-    // https://stackoverflow.com/a/7782507/10788820
-    for index, _ := range m.config.TargetEmails {
-        recipients[index] = mailjet.Recipient{
-            Email: m.config.TargetEmails[index],
-        }
-    }
+	// https://stackoverflow.com/a/38362784/10788820
+	recipients := make([]mailjet.Recipient, len(m.config.TargetEmails))
+	// https://stackoverflow.com/a/7782507/10788820
+	for index := range m.config.TargetEmails {
+		recipients[index] = mailjet.Recipient{
+			Email: m.config.TargetEmails[index],
+		}
+	}
 
-    email := &mailjet.InfoSendMail {
-      FromEmail: m.config.FromEmail,
-      FromName: m.config.FromName,
-      Subject: "",
-      TextPart: message,
-      Recipients: recipients,
-    }
+	email := &mailjet.InfoSendMail{
+		FromEmail:  m.config.FromEmail,
+		FromName:   m.config.FromName,
+		Subject:    "",
+		TextPart:   message,
+		Recipients: recipients,
+	}
 
-    mailjetClient := mailjet.NewMailjetClient(m.config.Mailjet.PublicApiKey, m.config.Mailjet.PrivateApiKey)
+	mailjetClient := mailjet.NewMailjetClient(m.config.Mailjet.PublicApiKey, m.config.Mailjet.PrivateApiKey)
 
-    i := 0
-    for i <= numRetries {
-        res, err := mailjetClient.SendMail(email)
-        if err != nil {
-            log.Printf("Error sending message: %v", err)
-        } else {
-            log.Printf("Successfully sent message: %v", res)
-            break;
-        }
-        i++
+	// Run in background thread so that retries don't hold up the execution of the program
+	go func() {
+		RunRetry(
+			func() *string {
+				res, err := mailjetClient.SendMail(email)
 
-        if i < numRetries {
-            log.Print("Retrying sending message")
-        }
-    }
+				if err != nil {
+					msg := fmt.Sprintf("Error sending message: %v", err)
+					return &msg
+				}
+
+				log.Printf("Successfully sent message: %v", res)
+				return nil
+			},
+			-1)
+	}()
 }
---
+++ retry.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"strconv"
+	"time"
+)
+
+// RunRetry retries the given function the given number of tries.
+// funcToRetry Should return nil if successful and an error message if it should be retried
+// numRetries is -1, then it'll retry forever.
+func RunRetry(funcToRetry func() *string, numRetries int) {
+	runForever := false
+	if numRetries < 1 {
+		runForever = true
+		numRetries = 1
+	}
+
+	retryBackoff := 1000 // Start at 1 second between retries
+	i := 0
+	for i <= numRetries {
+		err := funcToRetry()
+
+		if err != nil {
+			log.Printf("Retry: %v", *err)
+			log.Printf("Sleep for %v ms", retryBackoff)
+
+			backoffDuration, _ := time.ParseDuration(strconv.Itoa(retryBackoff) + "ms")
+			time.Sleep(backoffDuration)
+
+			// Double the time before the next retry
+			retryBackoff *= 2
+			// Only increase time between retries until it hits 5 minutes
+			if retryBackoff > 300000 {
+				retryBackoff = 300000
+			}
+
+			if !runForever {
+				i++
+			}
+		} else {
+			break
+		}
+	}
+
+}
